bac2e2682ae8d107cf94b0deade1d79e494c0b00
Arianna Masciolini <uzkamascio@gmail.com>
2022-08-14 18:12:04 +0200

    barebones version of interactive program

--- play.py
+++ play.py
@@ -1,8 +1,11 @@
+import os
 import sys
 import pyaudio
 import wave
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# import vlc
 
-def play_segment(audio_path, start,stop):
+def play_segment(audio_path, start, stop):
     wav = wave.open(audio_path, 'rb')
     py_audio = pyaudio.PyAudio()
     stream = py_audio.open(
@@ -19,6 +22,20 @@ def play_segment(audio_path, start,stop):
     stream.close()
     py_audio.terminate()
     wav.close()
+
+def play_segment_vlc(audio_path, start, stop):
+    segment_path = os.path.join(os.path.dirname(audio_path), 'tmp.wav')
+    ffmpeg_extract_subclip(audio_path, start, stop, segment_path)
+    # TODO: separate Windows command
+    os.system("nohup vlc --loop " + segment_path + " &") 
+
+    # tried using vlc lib but did not manage to launch with UI. Same with MPV
+    '''
+    media_player = vlc.MediaPlayer()
+    # media = vlc.Media(audio_path)
+    # media_player.set_media(media)
+    # media_player.play()
+    '''
 
 if __name__ == "__main__":
     audio_path = sys.argv[1]
--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,10 @@
+import os
 import sys
+from matplotlib import interactive
 import speech_recognition as sr
 import deepmultilingualpunctuation as dmp
+import csv
+from play import play_segment_vlc
 
 def transcribe_segment_sphinx(audio_path, start, stop, hints=[]):
     r = sr.Recognizer()
@@ -39,20 +43,61 @@ def transcribe_segment_google(audio_path, start, stop):
 def restore_punctuation(transcription):
     return dmp.PunctuationModel().restore_punctuation(transcription)
 
-if __name__ == "__main__":
-    cmd = sys.argv[1]
-    audio_path = sys.argv[2]
-    start = float(sys.argv[3])
-    stop = float(sys.argv[4])
+def interactive_transcribe_segment(cmd, audio_path, txt_path, start, stop, punct=True):
+    # transcribe
     if cmd == "sphinx":
         transcription = transcribe_segment_sphinx(audio_path, start, stop)
     elif cmd == "google":
         transcription = transcribe_segment_google(audio_path, start, stop)
-    if transcription: 
-        if len(sys.argv) > 5 and sys.argv[5] == "--no-punct":
-            print("\n\t>", transcription)
-        else:
-            print("\n\t>", restore_punctuation(transcription))
     else:
         print("Invalid transcription method!")
+        exit(1)
+
+    # restore punctuation
+    if punct:
+        try:
+            transcription = restore_punctuation(transcription)
+        except: # happens e.g. if transcription = "" 
+            pass
+
+    # write transcription to txt file
+    with open(txt_path, "w") as f:
+        f.write(transcription)
+
+    # play fragment; creates a tmp.wav file to be deleted/overwritten
+    play_segment_vlc(audio_path, start, stop) 
+
+    # open txt file in text editor
+    res = os.system("mousepad " + txt_path) # TODO: separate Windows command
+    if res == 0:
+        return
+
+def interactive_transcribe_trial(cmd, audio_path, punct=True):
+    # read segments from CSV
+    csv_path = os.path.join(os.path.dirname(audio_path), "segments.csv")
+    with open(csv_path) as f:
+        segments = csv.reader(f, delimiter="\t")
+        for (label,start_str,stop_str) in list(segments):
+            txt_path = os.path.join(
+                os.path.dirname(audio_path), 
+                start_str + "-" + stop_str + ".txt"
+            )
+            if not os.path.exists(txt_path): # start where you left off
+                if label == "speech":
+                    interactive_transcribe_segment(
+                        cmd, 
+                        audio_path, 
+                        txt_path,
+                        float(start_str), 
+                        float(stop_str), 
+                        punct
+                        )
+                elif label == "pause": # TODO: make pauses human-checkable
+                    pass
+        
+
+if __name__ == "__main__":
+    cmd = sys.argv[1]
+    audio_path = sys.argv[2]
+    interactive_transcribe_trial(cmd, audio_path)
         
